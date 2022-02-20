47299b0e428be150b579baa5318b6584b15f9131
Евгений Пермяков <evgenyu.permyakov@yandex.ru>
2022-02-20 17:58:19 +0500

    commit 17

--- homework.py
+++ homework.py
@@ -120,12 +120,15 @@ WORKOUTS = {
 
 def read_package(workout_type: str, data: list[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    TYPE_ERROR = (f'Выбрано {workout_type}: это неверный тип тренировки. '
-                  f'Попробуйте использовать один из '
-                  f'предложенных: {tuple(WORKOUTS)}')
-    DATA_ERROR = (f'Выбрано {workout_type}. '
-                  f'Ожидаемое число входных параметров {len(fields(WORKOUTS[workout_type]))}. '
-                  f'Было введено: {len(data)}.')
+    TYPE_ERROR = (
+        f'Выбрано {workout_type}: это неверный тип тренировки. '
+        f'Попробуйте использовать один из '
+        f'предложенных: {tuple(WORKOUTS)}')
+    DATA_ERROR = (
+        f'Выбрано {workout_type}. Ожидаемое число '
+        f'входных параметров {len(fields(WORKOUTS[workout_type]))}. '
+        f'Было введено: {len(data)}.'
+    )
     if workout_type not in WORKOUTS:
         raise ValueError(TYPE_ERROR.format(workout_type))
     if len(data) != len(fields(WORKOUTS[workout_type])):
